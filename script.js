8ffe25abc52762bf7e65465d66a3e5888d692c9e
José Victor <zoldyakopersonal@gmail.com>
2024-08-08 07:04:35 -0300

    Cells background now are changed with js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('container');
+    const container = document.getElementById('container')
 
 
     function generate_div_grid(res) {
-        let x, y, grid, cells;
+        let x, y, grid, cells
 
         // Creates divs and appends it to divs "column"
         for(y = 0; y < res; y++) {
 
-            grid = document.createElement("div");
-            grid.className = "column";
+            grid = document.createElement("div")
+            grid.className = "column"
 
             for(x = 0; x < res; x++) {
-                cells = document.createElement("div");
-                cells.className = "row";
-                cells.innerText = (x * res) + y + 1;
-                grid.appendChild(cells);
+                cells = document.createElement("div")
+                cells.className = "row"
+                grid.appendChild(cells)
+
+                cells.addEventListener('mouseover', (event) => {
+                    event.target.style.backgroundColor = 'black'
+                })
             }
             
-            container.appendChild(grid);
+            container.appendChild(grid)
         }
     }
 
-    generate_div_grid(8)
+    generate_div_grid(16)
+
 });
 
 
