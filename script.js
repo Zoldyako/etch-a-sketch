77bb456457ed3c4e630eed7adafff9b4da453169
José Victor <zoldyakopersonal@gmail.com>
2024-08-11 09:24:47 -0300

    Grid generator btn almost working properly

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('container')
+    
+    function generate_grid(res) {
 
-
-    function generate_div_grid(res) {
-        let x, y, grid, cells
-
+        let x, y, grid, cell
+        
         // Creates divs and appends it to divs "column"
         for(y = 0; y < res; y++) {
-
+            
             grid = document.createElement("div")
             grid.className = "column"
 
             for(x = 0; x < res; x++) {
-                cells = document.createElement("div")
-                cells.className = "row"
-                grid.appendChild(cells)
+                cell = document.createElement("div")
+                cell.className = "cell"
+                grid.appendChild(cell)
 
-                cells.addEventListener('mouseover', (event) => {
-                    event.target.style.backgroundColor = 'black'
+                cell.addEventListener('mousedown', (event) => {
+
+                    switch (event.button) {
+                        case 0:
+                            event.target.style.backgroundColor = 'black'
+                            break
+                        case 1:
+                            event.target.style.backgroundColor = 'white'
+                        case 2:
+                            event.target.style.backgroundColor = 'white'
+                            break
+                    }
                 })
             }
-            
+
             container.appendChild(grid)
         }
     }
 
-    generate_div_grid(16)
 
+    function canvas_size() {
+        res = prompt("What canvas size do you want? Max of 100")
+
+        if (res > 100) {
+            generate_grid(100)
+            return
+        }
+
+        generate_grid(res)
+    }
+
+
+    const container = document.getElementById('container')
+    const btn_grid_size = document.getElementById('grid-size')
+    const btn_erase = document.getElementById('erase-canvas')
+    
+    btn_grid_size.addEventListener('mousedown', canvas_size)
+    btn_erase.addEventListener('click', () => { })
+
+    generate_grid(10)
+    
 });
-
-
